e09070614b426cdfad149b7399562fbb67c6d954
RubiMiranda <91550567+RubiMiranda@users.noreply.github.com>
2021-09-30 21:40:27 -0500

    comprobacion de axiomas de numeros reales
    Roxana RMiranda Cruz

---
+++ Octavepropiedades.m
@@ -0,0 +1,56 @@
+%Octover Script       
+%Tilte            :Clasificacion de los numeros 
+%Descripcion      :Script para recordar conceptos de los numeros
+%Autor            :Roxana Rubi Miranda Cruz
+%Date             :20210930
+%Vercion          :1
+%Usage            :octave/Clasificacion/Documents
+%Notea            :Requiere aplicacion octave, usar su linea de comandos
+'Clear';
+C_numeros_Naturales="?={1 2 3 ...} si n>0?";
+C_numeros_Enteros ="? ={-n..., -2, -1, 0 ,1 ,2 ... n ?;}";
+C_numeros_Racionales="? = { m/n donde m,n n 0}";
+C_numeros_Irracionales= "i=(?n no deve ser expresada como ?,todas las raise que no son exactas}";
+C_numeros_Reales="? =(I,Q,Z,N)";
+%Propiedades de (Cerradura)
+a=3;
+b=7;
+c=5;
+%Cerradura
+a=3;
+b=7;
+a+b
+a*b
+%Propiedad asociativa 
+a=5;
+b=7;
+c=5;
+a+(b+c)
+(a+b)+c
+(a*b)+c
+a*(b+c)
+%propiedad conmutativa
+a+b
+b+a
+a*b
+b*a
+a=3;
+b=7;
+c=5;
+%Propiedad de elemento neutro
+a=3;
+b=7;
+a+0
+a*1
+%Propiedad  inversa
+a=3;
+a-(-a)
+a*1/a
+%propiedad distributiba
+a=3;
+b=7;
+c=5;
+a*(b+c)
+a*b+a*c
+
+
